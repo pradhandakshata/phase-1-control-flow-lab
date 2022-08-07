18177b2403b3a5ba22a51cac5bb56eff9dfd617d
DakshataPradhan <pradhandakshata1996@gmail.com>
2022-08-07 16:04:36 -0600

    commit tree 1a1f379960c2c6ed3551a1d24ec071e80ec84eb7

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ridedistance){
+  let result;
+  if (ridedistance <= 400){
+    result = 'This one is on me!'
+
+  } 
+  else if (ridedistance >400 && ridedistance<2000) {
+    result = 'That will be twenty bucks.'
+    
+  } 
+  else if (ridedistance>2000 && ridedistance<2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (ridedistance >2500){
+    result = 'No can do.'
+  }
+    return result;
+  }
+  
+function ternaryCheckCity(cityname){
+   let city =  cityname === "NYC" ? "Ok, sounds good." : "No go.";
+  return city;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+let tipquality;
+switch (tip){
+  case 'generous':
+    tipquality = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    tipquality = 'Thank you.';
+    break;
+  case 'thanks for everything':
+    tipquality = 'Bye.';
+    break;
+  default:
+    break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  return tipquality;
+  
 }
